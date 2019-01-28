939c9a1b271cdee45acbd02c38a48e415e717e5e
Qiqian <chuanmianyi1234@sina.com>
2019-01-28 23:26:03 +0800

    nothing want to say

---
+++ controllersandactions/ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllersAndActions.Controllers;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ViewSelectionTest()
+        {
+            //Arrange
+            //创建控制器
+            ExampleController target = new ExampleController();
+            //Action
+            //调用动作方法
+            ViewResult result = target.Index();
+            //Assert
+            //检查结果
+
+        }
+    }
+}
---
+++ controllersandactions/ControllersAndActions/Infrastructure/CustomRedirectResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Infrastructure
+{
+    public class CustomRedirectResult : ActionResult
+    {
+        public string Url { get; set; }
+
+        public override void ExecuteResult(ControllerContext P_Context)
+        {
+            string fullUrl = UrlHelper.GenerateContentUrl(Url, P_Context.HttpContext);
+            P_Context.HttpContext.Response.Redirect(fullUrl);
+        }
+    }
+}
